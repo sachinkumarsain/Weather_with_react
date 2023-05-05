83d071e28250049bdb1a3e537ecb886e3b631ae1
sachin <sachinsain21082004@gmail.com>
2023-05-05 14:23:58 +0530

    css work

--- src/Weather/Home.js
+++ src/Weather/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import "./Weather.css"
 
 function Home() {
     const [value, setValue] = useState('')
@@ -21,21 +22,23 @@ function Home() {
     return (
         <>
             <div className='container'>
-                <form onSubmit={submitForm}>
-                    <h2>Weather</h2>
-                    <input type='text' placeholder='Enter place' onChange={onchangeHandler}></input>
+               <div className='box'>
+               <form onSubmit={submitForm}>
+                    <h1>Weather</h1>
+                    <input type='text' placeholder='Enter place' onChange={onchangeHandler}></input><br/>
                     <button type='submit'> submit</button>
                 </form>
                 {
                     data ? (<div className='weather'>
                         <h1>{data.name}<span>
-                                <img src={`https://flagcdn.com/16x12/${data.sys.country.toLowerCase()}.png`}></img>
+                                <img src={`https://flagcdn.com/60x45/${data.sys.country.toLowerCase()}.png`}></img>
                         </span></h1>
                         <h2> Temp:-{data.main.temp}</h2>
                         <h3>Description:-{data.weather[0].description}</h3>
                     </div>)
                         : ("")
                 }
+               </div>
             </div>
         </>
     )
